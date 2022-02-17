03287cfbeb6f42bf57bb84e1623ed8e08b0b719d
Rif-7 <muhammadrifan974530@gmail.com>
2022-02-17 22:12:22 +0530

    add modal to save and time user

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import ImgContainer from "./components/ImgContainer";
+import SetUser from "./components/SetUser";
 
 import { initializeApp } from "firebase/app";
-import { getFireStore, addDoc, collection } from "firebase/firestore";
+import {
+  getFirestore,
+  addDoc,
+  collection,
+  serverTimestamp,
+  doc,
+  updateDoc,
+} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDPAG6GIZAvNy99fj5tHtjT5tm4uLIP5U4",
@@ -14,13 +22,51 @@ const firebaseConfig = {
   appId: "1:1001571650730:web:d87da5dc2223f8c63314d3",
 };
 
-// Initialize Firebase
 initializeApp(firebaseConfig);
 
+const db = getFirestore();
+
+// Initialize Firebase
+
 function App() {
+  const [userId, setUserId] = useState("");
+  const [showSetUp, setShowSetUp] = useState(true);
+
+  useEffect(() => {
+    if (userId === "") {
+      setShowSetUp(true);
+      return;
+    }
+    setShowSetUp(false);
+  }, [userId]);
+
+  function setEndTime() {
+    const userRef = doc(db, "timing", userId);
+    updateDoc(userRef, {
+      endTime: serverTimestamp(),
+    })
+      .then(() => console.log("timer ended"))
+      .catch((err) => console.log(err));
+  }
+
+  function timeUser(username) {
+    addDoc(collection(db, "timing"), {
+      username: username,
+      startTime: serverTimestamp(),
+      endTime: "",
+    })
+      .then((res) => {
+        setUserId(res.id);
+        console.log("user timed");
+      })
+      .catch((err) => console.log(err));
+  }
+
+  console.log(userId);
   return (
     <div>
       <Navbar />
+      {showSetUp ? <SetUser timeUser={timeUser} /> : null}
       <ImgContainer imgUrl="./imgs/wheres-waldo-1.jpg" />
     </div>
   );
---
+++ src/components/SetUser.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+function SetUser(props) {
+  const [username, setUsername] = useState("");
+
+  function updateUsername(e) {
+    setUsername(e.target.value);
+  }
+
+  return (
+    <div className="set-user">
+      <div className="content">
+        <p>You will be timed when you click start</p>
+        <label htmlFor="user-name">Username: </label>
+        <br></br>
+        <input type="text" id="user-name" onChange={updateUsername}></input>
+        <br></br>
+        <button className="start-btn" onClick={() => props.timeUser(username)}>
+          Start
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default SetUser;
