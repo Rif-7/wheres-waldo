108150802901b459828b7888dc511193e1fbccf5
Rif-7 <muhammadrifan974530@gmail.com>
2022-02-20 22:01:13 +0530

    add build folder for gh-pages

---
+++ precache-manifest.03da0bc5d93f62d84614330166c81af7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23aac312afd98e2e9430d51834bf0d27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79ee5db235a412ef554e",
+    "url": "/static/css/main.7408a77f.chunk.css"
+  },
+  {
+    "revision": "ae3e2d349e803035d4d8",
+    "url": "/static/js/2.19a53dbb.chunk.js"
+  },
+  {
+    "revision": "79ee5db235a412ef554e",
+    "url": "/static/js/main.17643c8b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
